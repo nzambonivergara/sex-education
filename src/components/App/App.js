1cbee2d94d245f0af15615b578bd15e054936b8f
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-09 15:55:18 -0700

    Add title and image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,6 +19,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Sex Education</h1>
+        <img src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABX_JCq57-TuTQRDPcumA7WlVQcLuflwx5fOmRLhQArm-6Z9fwrXHy1be91iW0nBu5ofCUJKNU0_4vfuSW-rART9C8S9fxgPsSM17DaROhANJbd5N.jpg?r=6e7" alt="sex education series"/>
       </div>
     );
   }
