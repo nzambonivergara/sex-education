3354e0bf576a8fe547d4dae8e98a445f389b3903
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-10 20:52:27 -0700

    Check response before parsing in api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,9 @@
-
 export const fetchQuestions = () => {
   return fetch('http://localhost:3000/api/v1/questions')
-    .then(response => response.json())
-
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} Error.`)
+      }
+      return response.json()
+    })
 }
