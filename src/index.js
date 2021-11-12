4b323a2628381c76096bfe35f46a63f1c99cb949
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-11 21:14:23 -0700

    Delete React logos and clean up html file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import './assets/condom-nav.png'
 
 
 ReactDOM.render(
