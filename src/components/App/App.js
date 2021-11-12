252c68705dbe1bef0613d9e518e45c649a2f051b
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-11 21:10:55 -0700

    Import NavBar component and add to render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { fetchQuestions } from '../../apiCalls';
 import { useState, useEffect } from 'react';
-import { Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Route, Switch, Link, NavLink, Redirect } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
 import Quiz from '../Quiz/Quiz';
 import './App.css';
 
@@ -11,54 +12,61 @@ const App = () => {
 
   useEffect(() => {
     fetchQuestions()
-      .then(data => setQuestions(data))
+      .then(data => setQuestions(data.slice(0,5)))
       .catch(error =>  setError(error.message));
   }, [])
 
   const checkAnswer = (answer, step) => {
     if (answer === questions[step].correctAnswer) {
       setScore(score + 1)
-      console.log(score)
     }
+    console.log(score)
+  }
+
+  const resetScore = () => {
+    setScore(0)
   }
 
   return (
       <div className="App">
-        <nav>
-          <NavLink to="/resources">
-            Resources
-          </NavLink>
-          <NavLink to="/quiz">
-            Quiz
-          </NavLink>
-          <NavLink to="/dashboard">
-            Dashboard
-          </NavLink>
-        </nav>
-        <h1>Sex Education</h1>
-        <img src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/SEX-EDUCATION-SEASON-4.jpg" alt="sex education series"/>
-        <Route
-         exact path="/"
-          render={() => {
-            return (
-              <>
-                <h2>Ready to play and see how much you know about sexual health?</h2>
-                <Link to="/quiz">YES!</Link>
-                <Link to="/dashboard">Skip for now!</Link>
-              </>
-            )
-          }}
-        />
-        <Route
-          exact path="/quiz"
-          render={() => <Quiz questions={ questions } checkAnswer={ checkAnswer } score={ score } />}
-        />
-        <Route
-          exact path="/dashboard"
-          render={() => {
-            return (<iframe width="370" height="200" src="https://www.youtube.com/embed/Xo3Cnfhf9Q8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>)
-          }}
-        />
+        <header>
+          <NavBar />
+          <h1>Sex Education</h1>
+        </header>
+        <Switch>
+          <Route
+           exact path="/"
+           render={() => <Redirect to="/home" /> }
+          />
+          <Route
+           exact path="/home"
+            render={() => {
+              return (
+                <>
+                  <h2>Take the Sexual Health Myth Debunking Quiz!</h2>
+                  <Link to="/quiz">YES!</Link>
+                  <footer>
+                    <Link to="/video">Watch Sex Education series video.</Link>
+                  </footer>
+                </>
+              )
+            }}
+          />
+          <Route
+            exact path="/quiz"
+            render={() => <Quiz questions={ questions } checkAnswer={ checkAnswer } score={ score } resetScore={ resetScore }/>}
+          />
+          <Route
+            exact path="/video"
+            render={() => {
+              return (
+                <>
+                  <iframe width="370" height="200" src="https://www.youtube.com/embed/Xo3Cnfhf9Q8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <Link to="/home">Back to Home</Link>
+                </>)
+            }}
+          />
+        </Switch>
       </div>
   )
 }
