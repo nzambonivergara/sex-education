b67cf580b43fb0437b7c8647e1b733b1054c8a06
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-11 21:12:27 -0700

    Add properties to disable the form inputs and next-button & create showResult and resetQuiz functions

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,15 +2,15 @@ import './Quiz.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Quiz = ({ questions, checkAnswer, score }) => {
+const Quiz = ({ questions, checkAnswer, score, resetScore }) => {
   const [ userAnswers, setUserAnswers ] = useState([])
   const [ step, setStep ] = useState(0)
-  const form = document.querySelector("form")
+  const [ buttonDisabled, setButtonDisabled ] = useState(true)
+  const [ formDisabled, setFormDisabled ] = useState(false)
 
   const handleChange = (answer) => {
-    form[0].disabled = true;
-    form[1].disabled = true;
-
+    setFormDisabled(true);
+    setButtonDisabled(false);
     setUserAnswers([ ...userAnswers, answer ])
     checkAnswer(answer, step)
   }
@@ -18,20 +18,51 @@ const Quiz = ({ questions, checkAnswer, score }) => {
   const changeQuestion = (event) => {
     event.preventDefault()
 
-    if (event.target.value === 'next' && userAnswers.length !== questions.length) {
-      form.reset()
-      form[0].disabled = false;
-      form[1].disabled = false;
+    if (event.target.value === 'next' && userAnswers[step+1] === undefined) {
+      setFormDisabled(false);
+      setButtonDisabled(true);
+      setStep(step + 1)
+    } else if (event.target.value === 'next' && userAnswers[step+1] !== undefined) {
+      setFormDisabled(true);
+      setButtonDisabled(false);
       setStep(step + 1)
     } else if (event.target.value === 'previous') {
       setStep(step - 1)
+      setFormDisabled(true);
+      setButtonDisabled(false);
     }
   }
 
+  const showResult = () => {
+    if (userAnswers[step] !== undefined && userAnswers[step] === questions[step].correctAnswer) {
+      return (
+        <>
+          <p>✅ Awesome job!</p>
+          <p>{questions[step].fact}</p>
+        </>
+      )
+    } else if (userAnswers[step] !== undefined && userAnswers[step] !== questions[step].correctAnswer) {
+      return (
+        <>
+          <p>❌ Let’s look at that one again!</p>
+          <p>{questions[step].fact}</p>
+        </>
+      )
+    }
+  }
+
+  const resetQuiz = () => {
+    setButtonDisabled(true);
+    setUserAnswers([])
+    setStep(0)
+    resetScore()
+  }
+
   return (
-    <form>
-      { userAnswers.length < questions.length ?
-        (<>
+    <section>
+      <h2>Myth Busting Quiz</h2>
+      { questions[step] ?
+        (<form>
           <h2>{questions[step].question}</h2>
           <label>
             <input
@@ -40,6 +71,7 @@ const Quiz = ({ questions, checkAnswer, score }) => {
               checked= { userAnswers[step] === true }
               value="true"
               onChange={() => handleChange(true)}
+              disabled={formDisabled}
             />
             True
           </label>
@@ -50,25 +82,36 @@ const Quiz = ({ questions, checkAnswer, score }) => {
               checked= { userAnswers[step] === false }
               value="false"
               onChange={() => handleChange(false)}
-              required
+              disabled={ formDisabled || userAnswers[step] }
             />
             False
           </label>
           <div>
             { step > 0 && <button className="" id="previous-button" value="previous" onClick={event => changeQuestion(event)}>Previous</button>}
-            { userAnswers.length <= questions.length - 1 && <button className="" id="next-button" value="next" onClick={event => changeQuestion(event)}>Next</button>}
+            <button
+              className=""
+              id="next-button"
+              value="next"
+              onClick={event => changeQuestion(event)}
+              disabled={buttonDisabled}
+            >
+              Next
+            </button>
           </div>
-          { userAnswers.length - 1 === step && userAnswers[step] === questions[step].correctAnswer && <p>Correct!</p> }
-          { userAnswers.length - 1 === step && userAnswers[step] !== questions[step].correctAnswer && <p>Incorrect!</p> }
-          { userAnswers.length - 1 === step && <p>{questions[step].fact}</p> }
-        </>)
-      : (
+          { showResult() }
+          <p>You have completed {userAnswers.length / questions.length * 100}% of the quiz!</p>
+        </form>
+      )
+      :
+      (
         <div>
           <h3>Amazing job! You completed the quiz.</h3>
           <p>Your score is {score}/{questions.length}</p>
+          <button onClick={ resetQuiz }>Retake Quiz</button>
+          <Link to="/home">Back to Home</Link>
         </div>
       )}
-    </form>
+    </section>
   )
 }
 
