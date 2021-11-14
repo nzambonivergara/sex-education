253aceb5257ed65e47c556aab0b263f988ff3c7f
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-14 15:29:02 -0700

    Transfer home page html to the Home component and add error handling for all Routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,25 +2,26 @@ import { fetchQuestions } from '../../apiCalls';
 import { useState, useEffect } from 'react';
 import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
+import Home from '../Home/Home';
 import Quiz from '../Quiz/Quiz';
+import Resources from '../Resources/Resources';
+import NotFound from '../NotFound/NotFound';
 import './App.css';
 
 const App = () => {
   const [ questions, setQuestions ] = useState([])
   const [ error, setError] = useState('')
   const [ score, setScore ] = useState(0)
-  const [ step, setStep ] = useState(0)
+
 
   useEffect(() => {
     fetchQuestions()
-      .then(data => {
-        setQuestions(data.slice(0,5))
-      })
+      .then(data => setQuestions(data.slice(0,15)))
       .catch(error =>  setError(error.message));
   }, [])
 
-  const checkAnswer = (answer, step) => {
-    if (answer === questions[step].correctAnswer) {
+  const checkAnswer = (answer, questionNumber) => {
+    if (answer === questions[questionNumber].correctAnswer) {
       setScore(score + 1)
     }
   }
@@ -30,7 +31,7 @@ const App = () => {
   }
 
   return (
-      <div className="App">
+      <div className="app">
         <header className="header">
           <NavBar resetScore={ resetScore }/>
           <h1>Sex Education</h1>
@@ -43,79 +44,31 @@ const App = () => {
           <Route
            exact path="/home"
             render={() => {
-              return (
-                <>
-                  <div className="quiz-call-to-action">
-                    <h2 className="call-to-action-question">
-                      Does wearing two condoms offer double protection?
-                    </h2>
-                    <Link to="/quiz" className="quiz-link" onClick={ resetScore }>
-                      Take the Myth Busting Quiz
-                      <span class="material-icons">
-                        arrow_right_alt
-                      </span>
-                    </Link>
-                  </div>
-                  <div className="fact-container">
-                    <h2 className="did-you-know">Did you know? 🤔</h2>
-                    <p className="fact">"{questions.length && questions[step].fact }"</p>
-                    <div className="arrows-container">
-                      <button
-                        className="arrow-button"
-                        disabled={ step ? false : true }
-                        onClick={() => setStep(step - 1)}
-                      >
-                        <span class="material-icons">
-                          arrow_back
-                        </span>
-                      </button>
-                      <button
-                        disabled={ step < questions.length - 1 ? false : true }
-                        className="arrow-button"
-                        onClick={() => setStep(step + 1)}
-                      >
-                        <span class="material-icons">
-                          arrow_forward
-                        </span>
-                      </button>
-                    </div>
-                  </div>
-                  {
-                  // <form className="submit-question-form">
-                  //   <h3 className="call-to-action-question">Do you have any sexual health questions?</h3>
-                  //   <textarea placeholder="Write your question here..."/>
-                  //   <button className="submit-button">Submit</button>
-                  // </form>
-                }
-                  <footer>
-                    <p>Please note: This app’s content is provided for general informational purposes only, and under no circumstances should be considered a substitute for professional medical advice, diagnosis or treatment from a qualified physician or healthcare provider.</p>
-                  </footer>
-                </>
-              )
+              if (!error) {
+                return <Home questions={ questions } resetScore={ resetScore } />
+              } else {
+                return <h2>Oops, something went wrong! Try again later!</h2>
+              }
             }}
           />
           <Route
             exact path="/quiz"
-            render={() => <Quiz questions={ questions } checkAnswer={ checkAnswer } score={ score } resetScore={ resetScore }/>}
-          />
-          <Route
-            exact path="/video"
             render={() => {
-              return (
-                <>
-                  <iframe width="370" height="200" src="https://www.youtube.com/embed/Xo3Cnfhf9Q8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  <Link to="/home">Back to Home</Link>
-                </>)
+              if (!error) {
+                return <Quiz questions={ questions } checkAnswer={ checkAnswer } score={ score } resetScore={ resetScore }/>
+              } else {
+                return <h2>Oops, something went wrong! Try again later!</h2>
+              }
             }}
           />
+          <Route exact path="/resources" component={Resources} />
+          <Route path="*" component={NotFound} />
         </Switch>
-        { error && <h2>{error.message}</h2>}
+        <footer className="footer">
+          <p>Please note: This app’s content is provided for general informational purposes only, and under no circumstances should be considered a substitute for professional medical advice, diagnosis or treatment from a qualified physician or healthcare provider.</p>
+        </footer>
       </div>
   )
 }
 
 export default App;
-
-// // <Link to="/video" className="video-link">
-//   Otis Milburn's Best Sex Ed Avice
-// </Link>
